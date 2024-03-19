20ce998fa1ce2cc97bff6511cdbd7d6f41c9ec4e
JanuDev <93274170+KizaKonpaku@users.noreply.github.com>
2024-03-18 20:46:35 -0400

    added vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: 'server',
-   i18n:{
+  i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
     routing: {
-        prefixDefaultLocale: false,
-    },
+      prefixDefaultLocale: false
+    }
   },
+  adapter: vercel(),
 });
